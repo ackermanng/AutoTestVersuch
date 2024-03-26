e8c315fa8bfdb7b3ea51ed0b77eae7ce64c9af1e
ackermanng <164891644+ackermanng@users.noreply.github.com>
2024-03-26 11:19:41 +0100

    Add files via upload

---
+++ Shell/multiTestfile.bat
@@ -0,0 +1,27 @@
+@echo off
+
+echo Da ist ein Mutlitestfile
+
+echo 1. lesen des Verzeichnisses
+
+dir
+
+echo 2. Was steht in der Datei Templates\File1.txt
+
+Setlocal EnableDelayedExpansion
+
+FOR /F "tokens=*" %%i IN (Templates\File1.txt) DO (
+
+  set v=%%i
+
+  echo "!v!"
+
+  echo "%%i"
+
+)
+
+echo 3. starte ein 2. Script
+
+call Programme\Testprogramm.bat > \log\log.txt
+
+exit 0
