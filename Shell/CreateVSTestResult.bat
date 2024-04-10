34ffbc33e4f2a3c58e140302e0d3e9ffefcf6a70
ackermanng <164891644+ackermanng@users.noreply.github.com>
2024-04-10 14:41:07 +0200

    Create CreateVSTestResult.bat

---
+++ Shell/CreateVSTestResult.bat
@@ -0,0 +1,12 @@
+@echo off
+
+del C:\agent\_work\_temp\TestResults\*.*
+
+mkdir C:\agent\_work\_temp\TestResults\ackermanng_DIP-NB0116_2024-04-10_13_49_53
+mkdir C:\agent\_work\_temp\TestResults\ackermanng_DIP-NB0116_2024-04-10_13_49_53\In
+mkdir C:\agent\_work\_temp\TestResults\ackermanng_DIP-NB0116_2024-04-10_13_49_53\In\23505ffc-b26f-4c62-a423-13a99ba271e4
+mkdir C:\agent\_work\_temp\TestResults\ackermanng_DIP-NB0116_2024-04-10_13_49_53\In\b8034fd5-4a9a-4684-9907-0dd33ff06fa8
+
+copy C:\agent\_work\1\s\Ergebnis\ackermanng_DIP-NB0116_2024-04-10_13_49_53.trx to C:\agent\_work\_temp\TestResults\ackermanng_DIP-NB0116_2024-04-10_13_49_53.trx
+copy C:\agent\_work\1\s\Ergebnis\In\Test1_log@SPRDMULTTS-0691_2024-03-28 15_22_42.mht to C:\agent\_work\_temp\TestResults\ackermanng_DIP-NB0116_2024-04-10_13_49_53\In\23505ffc-b26f-4c62-a423-13a99ba271e4\Test1_log@SPRDMULTTS-0691_2024-03-28 15_22_42.mht
+copy C:\agent\_work\1\s\Ergebnis\In\Testwrong_log@SPRDMULTTS-0691_2024-03-28 15_22_47.mht to C:\agent\_work\_temp\TestResults\ackermanng_DIP-NB0116_2024-04-10_13_49_53\In\b8034fd5-4a9a-4684-9907-0dd33ff06fa8\Testwrong_log@SPRDMULTTS-0691_2024-03-28 15_22_47.mht
