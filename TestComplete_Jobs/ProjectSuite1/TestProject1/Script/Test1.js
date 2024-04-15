ac23eb9c603c5a2aafb99c7d698cd679c468241b
ackermanng <164891644+ackermanng@users.noreply.github.com>
2024-04-15 08:59:55 +0200

    all TEstcompletejobs

---
+++ TestComplete_Jobs/ProjectSuite1/TestProject1/Script/Test1.js
@@ -0,0 +1,8 @@
+﻿function Test1()
+{
+  TestedApps.chrome.Run();
+  Browsers.Item(btChrome).Navigate("https://www.google.com/search?q=hallo&rlz=1C1GCEA_enDE1105DE1105&oq=hallo&gs_lcrp=EgZjaHJvbWUqDAgAEEUYOxixAxiABDIMCAAQRRg7GLEDGIAEMg0IARAuGNQCGLEDGIAEMg0IAhAuGIMBGLEDGIAEMgcIAxAAGIAEMg0IBBAAGIMBGLEDGIAEMgoIBRAAGLEDGIAEMgoIBhAAGLEDGIAEMgcIBxAAGIAEMg0ICBAuGIMBGLEDGIAEMgcICRAAGI8C0gEIMTc5N2owajeoAgiwAgE&sourceid=chrome&ie=UTF-8");
+  let browserWindow = Aliases.browser.BrowserWindow;
+  aqObject.CheckProperty(Aliases.chrome.pageHalloGoogleSuche.FindElement("//span[.='hal·lo']"), "contentText", cmpEqual, "hal·lo");
+  browserWindow.Close();
+}
